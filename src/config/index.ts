52c6c888eeb5ed6a59aa077a9a589fd2607b608b
LinCie <aldiofernanda@protonmail.com>
2024-12-27 21:35:09 +0700

    feat: add config env

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,1 +1,6 @@
 export * from "./lavalink";
+
+export const NODE_ENV = Bun.env.NODE_ENV!;
+export const DISCORD_TOKEN = Bun.env.DISCORD_TOKEN!;
+export const DEV_GUILD_ID = Bun.env.DEV_GUILD_ID!;
+export const BOT_CLIENT_ID = Bun.env.BOT_CLIENT_ID!;
