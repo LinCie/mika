9ac04ac162120ba5dc30a44c8467b037d58dee84
LinCie <aldiofernanda@protonmail.com>
2024-12-27 12:10:56 +0700

    feat: add simple ping commands

---
+++ src/commands/index.ts
@@ -0,0 +1,9 @@
+import * as ping from "./utility/ping";
+
+const commands = {
+  ping,
+};
+
+const commandsData = Object.values(commands).map((command) => command.data);
+
+export { commands, commandsData };
