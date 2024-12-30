2da8e1446b9a0b102af5ae3a18cf3047136d7c96
LinCie <aldiofernanda@protonmail.com>
2024-12-30 23:08:26 +0700

    feat: add embed reply when playing music and adding music

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,5 +1,7 @@
 export * from "./lavalink";
 
+export const GLOBAL_COLOR = 0xffe9f3;
+
 export const NODE_ENV = Bun.env.NODE_ENV!;
 export const DISCORD_TOKEN = Bun.env.DISCORD_TOKEN!;
 export const DEV_GUILD_ID = Bun.env.DEV_GUILD_ID!;
