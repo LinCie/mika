31a15582fda91605c8b1f584f64b8b1b8faeb5a8
LinCie <aldiofernanda@protonmail.com>
2025-02-05 21:00:39 +0700

    feat: remove user table

--- src/db/types.ts
+++ src/db/types.ts
@@ -1,20 +1,15 @@
 import type { ColumnType } from "kysely";
 export type Generated<T> = T extends ColumnType<infer S, infer I, infer U>
-	? ColumnType<S, I | undefined, U>
-	: ColumnType<T, T | undefined, T>;
+  ? ColumnType<S, I | undefined, U>
+  : ColumnType<T, T | undefined, T>;
 export type Timestamp = ColumnType<Date, Date | string, Date | string>;
 
 export type Playlist = {
-	id: Generated<number>;
-	userId: string;
-	name: string;
-	songs: Generated<string[]>;
-};
-export type User = {
-	id: string;
-	username: string;
+    id: Generated<number>;
+    userId: string;
+    name: string;
+    songs: Generated<string[]>;
 };
 export type DB = {
-	Playlist: Playlist;
-	User: User;
+    Playlist: Playlist;
 };
