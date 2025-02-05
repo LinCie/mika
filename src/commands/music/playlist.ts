ee2f3a9c265564056b6ee4fa0056fd1eddfbcdd9
LinCie <aldiofernanda@protonmail.com>
2025-02-05 22:11:52 +0700

    feat: add add track command

--- src/commands/music/playlist.ts
+++ src/commands/music/playlist.ts
@@ -23,6 +23,7 @@ import {
 	IsPlayerExist,
 	IsPlayerCurrent,
 } from "@/guards";
+import { LoadType, type Track } from "shoukaku";
 
 @Discord()
 @SlashGroup({ name: "playlist", description: "Playlist Manager" })
@@ -43,8 +44,10 @@ class Playlist {
 		client: Mika,
 	) {
 		const member = interaction.member as GuildMember;
+
 		try {
 			const playlist = await client.playlist.createPlaylist(name, member);
+
 			const embed = client.embed.createMessageEmbedWithAuthor(
 				`Created playlist with name \'${playlist.name}\'`,
 				member,
@@ -53,8 +56,106 @@ class Playlist {
 			await client.interaction.replyEmbed(interaction, embed);
 		} catch (error) {
 			client.pino.error(error);
+
 			const embed = client.embed.createMessageEmbedWithAuthor(
 				"There is an error while adding playlist",
+				member,
+				EMBEDTYPE.ERROR,
+			);
+			await client.interaction.replyEmbed(interaction, embed);
+		}
+	}
+
+	@Slash({ description: "Create a playlist" })
+	@Guard(DeferReply, IsPlayerExist)
+	async add(
+		@SlashOption({
+			name: "playlist",
+			description: "The name of the playlist",
+			required: true,
+			type: ApplicationCommandOptionType.String,
+		})
+		name: string,
+
+		@SlashOption({
+			name: "name",
+			description: "The song url",
+			required: false,
+			type: ApplicationCommandOptionType.String,
+		})
+		url: string | undefined,
+
+		interaction: CommandInteraction,
+		client: Mika,
+		data: { player: PlayerManager; member: GuildMember },
+	) {
+		const { player, member } = data;
+
+		try {
+			let song: Track | Track[] | undefined;
+			if (!url) {
+				song = player.queue.getCurrent();
+			} else {
+				const result = await player.searchMusic(url, "url");
+
+				switch (result?.loadType) {
+					case LoadType.TRACK:
+						song = result.data;
+						break;
+					case LoadType.PLAYLIST:
+						song = result.data.tracks;
+						break;
+					default:
+						throw new Error("Invalid URL");
+				}
+			}
+
+			const playlist = await client.playlist.getPlaylistByName(name);
+
+			if (playlist.userId !== member.user.id) {
+				const embed = client.embed.createMessageEmbedWithAuthor(
+					"You're not the owner of this playlist",
+					member,
+					EMBEDTYPE.ERROR,
+				);
+				await client.interaction.replyEmbed(interaction, embed);
+				return;
+			}
+
+			if (!song) {
+				throw new Error("Empty result");
+			}
+
+			if (Array.isArray(song)) {
+				for (const track of song) {
+					playlist.songs.push(track.info.uri!);
+				}
+			} else {
+				playlist.songs.push(song.info.uri!);
+			}
+
+			await client.playlist.addTrackToPlaylist(playlist.id, playlist);
+
+			if (Array.isArray(song)) {
+				const embed = client.embed.createMessageEmbedWithAuthor(
+					`Successfully added your playlist to playlist \'${playlist.name}\'`,
+					member,
+					EMBEDTYPE.SUCCESS,
+				);
+				await client.interaction.replyEmbed(interaction, embed);
+			} else {
+				const embed = client.embed.createMessageEmbedWithAuthor(
+					`Successfully added ${song} to playlist \'${playlist.name}\'`,
+					member,
+					EMBEDTYPE.SUCCESS,
+				);
+				await client.interaction.replyEmbed(interaction, embed);
+			}
+		} catch (error) {
+			client.pino.error(error);
+
+			const embed = client.embed.createMessageEmbedWithAuthor(
+				"There is an error while adding music to playlist",
 				member,
 				EMBEDTYPE.ERROR,
 			);
