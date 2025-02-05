7cb08a41afd62cd3b58482677229ff251550353e
LinCie <aldiofernanda@protonmail.com>
2025-02-05 21:14:30 +0700

    feat: add better interaction error handling

--- src/instances/manager/InteractionManager.ts
+++ src/instances/manager/InteractionManager.ts
@@ -3,52 +3,72 @@ import type {
 	ButtonInteraction,
 	CommandInteraction,
 	EmbedBuilder,
-	InteractionResponse,
-	Message,
 	MessageActionRowComponentBuilder,
 	MessageCreateOptions,
 	MessagePayloadOption,
 	TextChannel,
 } from "discord.js";
+import type { Mika } from "../Mika";
 
 class InteractionManager {
+	private readonly client: Mika;
+
+	constructor(client: Mika) {
+		this.client = client;
+	}
+
 	public replyEmbedWithButton(
 		interaction: CommandInteraction | ButtonInteraction,
 		embed: EmbedBuilder,
 		buttons: ActionRowBuilder<MessageActionRowComponentBuilder>,
 		options?: MessagePayloadOption | MessageCreateOptions,
-	): Promise<Message<boolean>> | Promise<InteractionResponse<boolean>> {
-		if (interaction.isButton()) {
-			return interaction.update({
+	) {
+		try {
+			if (interaction.isButton()) {
+				return interaction.update({
+					components: [buttons],
+					embeds: [embed],
+					options,
+				});
+			}
+			return interaction.editReply({
 				components: [buttons],
 				embeds: [embed],
 				options,
 			});
+		} catch (error) {
+			this.client.pino.error(error);
+			throw error;
 		}
-		return interaction.editReply({
-			components: [buttons],
-			embeds: [embed],
-			options,
-		});
 	}
 
 	public replyEmbed(
 		interaction: CommandInteraction,
 		embed: EmbedBuilder,
 		options?: MessagePayloadOption | MessageCreateOptions,
-	): Promise<Message<boolean>> {
-		return interaction.editReply({
-			embeds: [embed],
-			options,
-		});
+	) {
+		try {
+			return interaction.editReply({
+				embeds: [embed],
+				options,
+			});
+		} catch (error) {
+			this.client.pino.error(error);
+			throw error;
+		}
 	}
 
 	public sendEmbed(
 		channel: TextChannel,
 		embed: EmbedBuilder,
 		options?: MessagePayloadOption | MessageCreateOptions,
-	): Promise<Message<boolean>> {
-		return channel.send({ embeds: [embed], options });
+	) {
+		try {
+			return channel.send({ embeds: [embed], options });
+		} catch (error) {
+			this.client.pino.error(error);
+			throw error;
+		}
 	}
 }
 
