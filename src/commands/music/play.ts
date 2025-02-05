c4dcb772a58aa41f166b423592dbbbe698b0bd6c
LinCie <aldiofernanda@protonmail.com>
2025-02-05 21:17:22 +0700

    feat: add better error handling for play command

--- src/commands/music/play.ts
+++ src/commands/music/play.ts
@@ -44,65 +44,76 @@ class Play {
 		data: { player: PlayerManager; member: GuildMember },
 	): Promise<void> {
 		const { player, member } = data;
-		const result = await player.searchMusic(query, method);
 
-		switch (result?.loadType) {
-			case LoadType.SEARCH: {
-				const track = result.data.shift();
-				if (track) {
-					player.queue.addTrack(track);
-					const embed = client.embed.createAddTrackEmbed(track, member);
-					await client.interaction.replyEmbed(interaction, embed);
+		try {
+			const result = await player.searchMusic(query, method);
+
+			switch (result?.loadType) {
+				case LoadType.SEARCH: {
+					const track = result.data.shift();
+					if (track) {
+						player.queue.addTrack(track);
+						const embed = client.embed.createAddTrackEmbed(track, member);
+						await client.interaction.replyEmbed(interaction, embed);
+					}
+					break;
 				}
-				break;
-			}
 
-			case LoadType.TRACK: {
-				const track = result.data;
-				if (track) {
-					player.queue.addTrack(track);
-					const embed = client.embed.createAddTrackEmbed(track, member);
-					await client.interaction.replyEmbed(interaction, embed);
+				case LoadType.TRACK: {
+					const track = result.data;
+					if (track) {
+						player.queue.addTrack(track);
+						const embed = client.embed.createAddTrackEmbed(track, member);
+						await client.interaction.replyEmbed(interaction, embed);
+					}
+					break;
 				}
-				break;
-			}
 
-			case LoadType.PLAYLIST: {
-				const tracks = result.data.tracks;
-				if (tracks) {
-					const embed = client.embed.createAddPlaylistEmbed(
-						tracks,
-						result,
-						member,
+				case LoadType.PLAYLIST: {
+					const tracks = result.data.tracks;
+					if (tracks) {
+						const embed = client.embed.createAddPlaylistEmbed(
+							tracks,
+							result,
+							member,
+						);
+						player.queue.addTracks(tracks);
+						await client.interaction.replyEmbed(interaction, embed);
+					}
+					break;
+				}
+
+				case LoadType.EMPTY: {
+					const embed = client.embed.createMessageEmbed(
+						`No result found with query "${query}"`,
+						EMBEDTYPE.ERROR,
 					);
-					player.queue.addTracks(tracks);
-					await client.interaction.replyEmbed(interaction, embed);
+					await client.interaction.replyEmbed(interaction, embed, {
+						ephemeral: true,
+					});
+					break;
 				}
-				break;
-			}
 
-			case LoadType.EMPTY: {
-				const embed = client.embed.createMessageEmbed(
-					`No result found with query "${query}"`,
-					EMBEDTYPE.ERROR,
-				);
-				await client.interaction.replyEmbed(interaction, embed, {
-					ephemeral: true,
-				});
-				break;
+				case LoadType.ERROR: {
+					const embed = client.embed.createMessageEmbed(
+						"An error had occured. Please try again later </3",
+						EMBEDTYPE.ERROR,
+					);
+					await client.interaction.replyEmbed(interaction, embed, {
+						ephemeral: true,
+					});
+					client.pino.error(result.data.message, result.data.cause);
+					break;
+				}
 			}
-
-			case LoadType.ERROR: {
-				const embed = client.embed.createMessageEmbed(
-					"An error had occured. Please try again later </3",
-					EMBEDTYPE.ERROR,
-				);
-				await client.interaction.replyEmbed(interaction, embed, {
-					ephemeral: true,
-				});
-				client.pino.error(result.data.message, result.data.cause);
-				break;
-			}
+		} catch (error) {
+			client.pino.error(error);
+			const embed = client.embed.createMessageEmbedWithAuthor(
+				"There is an error while trying to play music",
+				member,
+				EMBEDTYPE.ERROR,
+			);
+			await client.interaction.replyEmbed(interaction, embed);
 		}
 	}
 }
