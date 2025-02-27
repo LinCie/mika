abe05f20ab1af45678105d5f69f65d4d4bcffbbe
LinCie <aldiofernanda@protonmail.com>
2025-02-27 12:51:03 +0700

    feat: add resume command

---
+++ src/commands/music/resume.ts
@@ -0,0 +1,36 @@
+import {
+    ChatInputCommandInteraction,
+    GuildMember,
+    SlashCommandBuilder,
+} from 'discord.js'
+import { Command, EMBEDTYPE, Mika, PlayerManager } from '@/instances'
+import { IsInVoiceChannel, IsPlayerCurrent, IsPlayerExist } from '@/middlewares'
+
+const data = new SlashCommandBuilder()
+    .setName('resume')
+    .setDescription('Resume current player')
+
+class Resume extends Command {
+    constructor() {
+        super(data)
+        this.use(IsInVoiceChannel, IsPlayerExist, IsPlayerCurrent)
+    }
+
+    async command(
+        client: Mika,
+        interaction: ChatInputCommandInteraction,
+        context: { player: PlayerManager; member: GuildMember }
+    ) {
+        const { player, member } = context
+
+        await player.resumeMusic()
+        const embed = client.embed.createMessageEmbedWithAuthor(
+            '🎶 **Mika** has been resumed 🎶',
+            member,
+            EMBEDTYPE.SUCCESS
+        )
+        await client.interaction.replyEmbed(interaction, embed)
+    }
+}
+
+export default Resume
