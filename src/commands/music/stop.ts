a2f5505726528cef65f386b1f77842782ceeb7cb
LinCie <aldiofernanda@protonmail.com>
2025-02-27 12:37:10 +0700

    feat: add stop command

---
+++ src/commands/music/stop.ts
@@ -0,0 +1,27 @@
+import { ChatInputCommandInteraction, SlashCommandBuilder } from 'discord.js'
+import { Command, Mika, PlayerManager } from '@/instances'
+import { IsInVoiceChannel, IsPlayerCurrent, IsPlayerExist } from '@/middlewares'
+
+const data = new SlashCommandBuilder()
+    .setName('stop')
+    .setDescription('Stop current player')
+
+class Stop extends Command {
+    constructor() {
+        super(data)
+        this.use(IsInVoiceChannel, IsPlayerExist, IsPlayerCurrent)
+    }
+
+    async command(
+        _client: Mika,
+        interaction: ChatInputCommandInteraction,
+        context: { player: PlayerManager }
+    ) {
+        const { player } = context
+
+        await player.player?.destroy()
+        await interaction.deleteReply()
+    }
+}
+
+export default Stop
