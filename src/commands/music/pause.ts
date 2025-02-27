4b86d6dd45576ec91c382d588bd121edbfae6238
LinCie <aldiofernanda@protonmail.com>
2025-02-27 12:50:14 +0700

    feat: add pause command

---
+++ src/commands/music/pause.ts
@@ -0,0 +1,36 @@
+import {
+    ChatInputCommandInteraction,
+    GuildMember,
+    SlashCommandBuilder,
+} from 'discord.js'
+import { Command, EMBEDTYPE, Mika, PlayerManager } from '@/instances'
+import { IsInVoiceChannel, IsPlayerCurrent, IsPlayerExist } from '@/middlewares'
+
+const data = new SlashCommandBuilder()
+    .setName('pause')
+    .setDescription('Pause current player')
+
+class Pause extends Command {
+    constructor() {
+        super(data)
+        this.use(IsInVoiceChannel, IsPlayerExist, IsPlayerCurrent)
+    }
+
+    async command(
+        client: Mika,
+        interaction: ChatInputCommandInteraction,
+        context: { player: PlayerManager; member: GuildMember }
+    ) {
+        const { player, member } = context
+
+        await player.pauseMusic()
+        const embed = client.embed.createMessageEmbedWithAuthor(
+            '🎶 **Mika** has been paused 🎶',
+            member,
+            EMBEDTYPE.SUCCESS
+        )
+        await client.interaction.replyEmbed(interaction, embed)
+    }
+}
+
+export default Pause
